9ef91cd9c0dc89e294997304ec304197ac7854a0
Grisha Vartanyan <grisha@vartanyan.com>
2021-11-15 13:58:04 +0100

    Added question thread, message passing to client

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
 extern crate sha1;
 
+use std::env::current_dir;
 use std::ops::Deref;
+use std::sync::atomic::AtomicU8;
 use std::{env, panic};
 use std::path::Path;
 use std::fs;
 use std::time::SystemTime;
 use std::hash::{Hash, Hasher};
-use std::thread;
+use std::thread::{self, current};
 
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 const IP_PORT: &str = "localhost:7878";
+const ANSWER_TIME_MS: u32 = 30 * 1000;
 
+const T_MAIN: &str = "MAIN";
+const T_Q_SETTER: &str = "Q SETTER";
 
 #[derive(Debug)]
 struct Question {
@@ -25,7 +30,6 @@ struct Question {
 impl Question {
     // https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_servers
     fn ws_packet(&self) -> Vec<u8> {  // TODO: think about caching
-
         let len: usize = self.text.len() + 2;  // payload length
         let mut res: Vec<u8> = Vec::with_capacity(len + 12);
 
@@ -110,44 +114,78 @@ fn main() {
     let page = fs::read_to_string("page.html").unwrap();
     //let mut users: HashMap<u32, User> = HashMap::new();
 
+    // set up game data and thread
+    let current_q = Arc::new(AtomicU8::new(0));
+    let q_setter_handle = {
+        let current_q = Arc::clone(&current_q);
+        let q_count = questions_arc.len();
+        thread::spawn(move || {
+            println!("[{}]: PRESS RETURN TO START QUESTIONS", T_Q_SETTER);
+            let mut buffer = String::new();
+            std::io::stdin().read_line(&mut buffer).unwrap();  // wait for user input
+            println!("[{}]: QUESTIONS STARTING", T_Q_SETTER);
+
+            loop {
+                current_q.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+                println!("[{}]: MOVED TO QUESTION {:?}", T_Q_SETTER, current_q);
+                thread::sleep_ms(ANSWER_TIME_MS);
+            }
+        })
+    };
+
     // start server
     let listener = TcpListener::bind(IP_PORT).unwrap();
+    let mut thread_handles = Vec::with_capacity(50);
     for stream in listener.incoming() {
         let mut stream = stream.unwrap();
-        let questions_arc = Arc::clone(&questions_arc);
 
         let mut buffer = [0; 1024];
         stream.read(&mut buffer).unwrap();
         let beg_text = String::from_utf8_lossy(&buffer[..]).deref().to_string();
-        println!("-------RECIEVE------\n{}", beg_text);
+        //println!("-------RECIEVE------\n{}", beg_text);
 
-        if     !beg_text.starts_with("GET /")      {println!("NON-GET REQ")}
-        else if beg_text.starts_with("GET / HTTP") {send_init_page(&mut stream, &page);}
-        else if beg_text.starts_with("GET /ws HTTP") {
+        if !beg_text.starts_with("GET /") {
+            println!("[{}]: Recieved non-GET request!", T_MAIN);
+        } else if beg_text.starts_with("GET / HTTP") {
+            println!("[{}]: Root GET request; serving webpage", T_MAIN);
+            send_init_page(&mut stream, &page);
+        } else if beg_text.starts_with("GET /ws HTTP") {
+            println!("[{}]: WS GET request; starting thread", T_MAIN);
             // start WS -> new thread, yada yada
-            thread::spawn( || {
+            let questions_arc = Arc::clone(&questions_arc);
+            let current_q = Arc::clone(&current_q);
+
+            let handle = thread::spawn( || {
                 let addr = stream.peer_addr().unwrap();
-                println!("---===   NEW THREAD STARTED   ===---\n[serving {:?}]\n", addr);
+                println!("[{:?}]: Thread started", addr);
                 handle_connection(
                     stream,
                     questions_arc,
                     beg_text,
+                    current_q,
                     //&mut users
                 );
-                println!("---===   THREAD DEAD   ===---\n[serving {:?}]\n", addr);
+                println!("{:?}]: thread ended", addr);
             });
-        } else {println!("STRANGE GET REQ")}
-
+            thread_handles.push(handle);
+        } else {
+            println!("[{}]: Failed to parse GET req", T_MAIN);
+        }
     }
+
+    q_setter_handle.join();
+    thread_handles.into_iter().for_each(|x| {x.join().unwrap();});
+    println!("[{}]: ALL THREADS JOINED, EXIT", T_MAIN);
 }
 
 fn handle_connection(
     mut stream: TcpStream,
     questions_arc: Arc<Vec<Question>>,
     request_text: String,
+    cur_q: Arc<AtomicU8>,
     //users: &mut HashMap<u32, User>,
 ) {
-    
+    let addr = stream.peer_addr().unwrap(); 
     // connection to websocket, start thing
     let key_in = &request_text.split("Sec-WebSocket-Key: ")
         .nth(1).expect("no ws key")[..24];
@@ -159,39 +197,44 @@ fn handle_connection(
         ws_response_hash(key_in),
     );
     send_bytes_to_stream(&mut stream, response.as_bytes());
-    println!("-------SEND------\n{}", response);
+    println!("[{}]: Sent HTTP 101", addr);
+    //println!("-------SEND------\n{}", response);
 
-    for q in questions_arc.iter() {
+    for (i, q) in questions_arc.iter().enumerate() {
+        //thread::sleep_ms(300);
         let send_data = &q.ws_packet();
         send_bytes_to_stream(&mut stream, send_data);
-        println!("-------SEND------\n{:?}\n", send_data);
+        //println!("-------SEND------\n{:?}\n", send_data);
+        println!("[{}]: Sent Question", addr);
         
-        thread::sleep_ms(100);
-        let mut buf = [0; 8];
+        let mut buf = [0; 10];
         stream.read(&mut buf).unwrap();
-        println!("Revieced WS: {:?}", buf);
+        //println!("Revieced WS: {:?}", buf);
+        println!("[{}]: Recieved answer", addr);
         
-        if buf[0] != 129 {continue;}  // fin, text
-        if buf[1] != 130 {continue;}  // mask, len=2
+        if buf[0] != 129 {break;}  // fin, text
+        if buf[1] != 130 {break;}  // mask, len=2
+        if buf[8] != 0   {break;}  // message should only be 8 long
+        if buf[9] != 0   {break;}
 
-        let qID = (buf[2] ^ buf[6]) - b'0';
-        let aID = (buf[3] ^ buf[7]) - b'0';
+        let q_id = (buf[2] ^ buf[6]) - b'0';
+        let a_id = (buf[3] ^ buf[7]) - b'0';
 
-        if qID != q.id {println!("WRONG QUESTION ANSWERED");continue;}  // TODO
+        if q_id != q.id {
+            println!("[{}]: Answer was for wrong question!", addr);
+            continue;
+        }  // TODO!!!
 
-        println!("Guessed {} for question {}.", aID, qID);
-        println!("Correct answer was {}. You guess was {}.\n", q.ans, q.ans==aID);
+        println!("Guessed {} for question {}.", a_id, q_id);
+        println!("Correct answer was {}. You guess was {}.\n", q.ans, q.ans==a_id);
     }
-    println!("all questions finished!");
-    loop {};
 
-    /*pid = random::<u32>();
-    user = User {
-        pid,
-        last_seen: SystemTime::now(),
-        score: 0,
-    };
-    users.insert(pid, user);*/
+    println!("All questions finished!");
+    send_bytes_to_stream(&mut stream, b"\x81\x24Game Finished! Closing Connection...");
+    println!("SENT: {:?}", b"\x81\x24Game Finished! Closing Connection...");
+
+    thread::sleep_ms(1000);
+    stream.shutdown(std::net::Shutdown::Both).expect("Shutdown failed");
 }
 
 
@@ -203,7 +246,7 @@ fn send_init_page(stream: &mut TcpStream, page: &String) {
         page,
     );
     send_bytes_to_stream(stream, response.as_bytes());
-    println!("-------SEND------\n{}[HTML PAGE]\n", &response[..40]);
+    //println!("-------SEND------\n{}[HTML PAGE]\n", &response[..40]);
 }
 
 
@@ -218,5 +261,3 @@ fn send_bytes_to_stream(stream: &mut TcpStream, buf: &[u8]) {
     stream.write(buf).unwrap();
     stream.flush().unwrap();
 }
-
-
